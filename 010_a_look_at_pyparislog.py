22711f0b9a712cb89249ec87ecae7f5453bfa266
giadarol <giovanni.iadarola@cern.ch>
2018-09-24 12:03:30 +0200

    Status

--- 010_a_look_at_pyparislog.py
+++ 010_a_look_at_pyparislog.py
@@ -19,7 +19,21 @@ sim_folder = '../test2_on_HPC_1slot/004_multibunch_with_ecloud'
 
 with open(sim_folder+'/pyparislog.txt') as fid:
     pyploglns = fid.readlines()
-  
+
+#check that pyparislog is proper
+flag_startsim = map(lambda s: 'PyPARIS simulation -- multiturn parallelization' in s, pyploglns)
+if np.sum(flag_startsim)!=1:
+    print('sum(flag_startsim) is %d'%np.sum(flag_startsim))
+    raise ValueError('Problem in pyparislog! Did you start from an empty file')
+
+dict_config = {}
+config_params = 'N_cores N_pieces_per_transfer N_buffer_float_size N_buffer_int_size N_parellel_rings N_nodes_per_ring'.split()
+for par in config_params:
+    flag_par = map(lambda s: par+' = ' in s, pyploglns)
+    assert np.sum(flag_par)==1
+    i_par = np.where(flag_par)[0][0]
+    dict_config[par] = int(pyploglns[i_par].split()[-1])
+
 t_list = []
 ibun_list = []
 iturn_list = []
@@ -38,10 +52,23 @@ for ln in pyploglns:
         t_list.append(tt)
         iturn_list.append(iturn)
 
+ibun_arr = np.array(ibun_list)
+t_arr = np.array(t_list)
+iturn_arr = np.array(iturn_list)
+
+iter_turn_steps = np.array([0] + list(np.where(np.diff(iturn_list))[0]+1))
+iturn_steps = iturn_arr[iter_turn_steps]
+tturn_steps = t_arr[iter_turn_steps] 
+
+n_turns_steps = list(set(np.diff(iturn_steps)))
+assert len(n_turns_steps)==1
+n_turns_steps = n_turns_steps[0]
+
+avgt_turn_steps = np.diff(tturn_steps)/n_turns_steps
 
 
 import matplotlib.pyplot as plt
-#~ plt.close('all')
+plt.close('all')
 plt.figure(figsize=(8,1.5*6))
 ax1=plt.subplot(4,1,1)
 plt.plot(np.diff(t_list), '.-')
@@ -52,6 +79,7 @@ plt.plot(ibun_list, '.-')
 ax2.set_ylabel('"Bunch" at CPU 0')
 ax3=plt.subplot(4,1,3, sharex=ax1)
 plt.plot(iturn_list, '.-')
+ax3.plot(iter_turn_steps, iturn_steps, '.r')
 ax3.set_ylabel('Turn at CPU 0')
 ax4=plt.subplot(4,1,4, sharex=ax1)
 plt.plot((np.array(t_list)-t_list[0])/3600., '.-')
